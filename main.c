283b9e306c8809d7e76990fa1cc3fed9cf3f9c44
Gledex <pstalker2@gmail.com>
2021-04-18 19:44:59 +0700

    Update main.c to move

--- main.c
+++ main.c
@@ -1,33 +1,6 @@
+#include "/home/pavel/Документы/TRPO/chessviz-Gledex/src/libchess/move.h"
+
 #include <stdio.h>
-#include <string.h>
-
-void start_cell(char cell[][9])
-{
-    char first_row[9] = {"rnbqkbnr\0"};
-    char sec_row[9] = {"pppppppp\0"};
-    char sev_row[9] = {"PPPPPPPP\0"};
-    char ei_row[9] = {"RNBQKBNR\0"};
-    for (int i = 0; i < 9; i++) {
-        cell[0][i] = first_row[i];
-    }
-    for (int i = 0; i < 9; i++) {
-        cell[1][i] = sec_row[i];
-    }
-    for (int i = 0; i < 9; i++) {
-        cell[6][i] = sev_row[i];
-    }
-    for (int i = 0; i < 9; i++) {
-        cell[7][i] = ei_row[i];
-    }
-}
-
-void display_cell(char cell[8][9])
-{
-    printf("\n");
-    for (int i = 0; i < 8; i++) {
-        printf("\t\t%s\n", cell[i]);
-    }
-}
 
 int main()
 {
@@ -41,5 +14,9 @@ int main()
                "        \0",
                "        \0"};
     start_cell(cell);
+    display_cell(cell);
+    char turn[255] = {'\0'};
+    fgets(turn, 6, stdin);
+    move(turn, 5, cell[0]);
     display_cell(cell);
 }
