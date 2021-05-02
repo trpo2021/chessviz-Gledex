7eecfe49163c8603899aeb4e3fe137b5eb5746fe
Gledex <pstalker2@gmail.com>
2021-05-02 20:07:01 +0700

    removing display and start from move

--- src/libchess/move.c
+++ src/libchess/move.c
@@ -49,39 +49,6 @@ void parse(char str[], int n, cord* start, cord* end)
         end->num = atoi(&str[i]) - 1;
     }
 }
-void start_cell(char cell[][9])
-{
-    char first_row[9] = {"rnbqkbnr\0"};
-    char sec_row[9] = {"pppppppp\0"};
-    char sev_row[9] = {"PPPPPPPP\0"};
-    char ei_row[9] = {"RNBQKBNR\0"};
-    for (int i = 0; i < 9; i++) {
-        cell[0][i] = first_row[i];
-    }
-    for (int i = 0; i < 9; i++) {
-        cell[1][i] = sec_row[i];
-    }
-    for (int i = 0; i < 9; i++) {
-        cell[6][i] = sev_row[i];
-    }
-    for (int i = 0; i < 9; i++) {
-        cell[7][i] = ei_row[i];
-    }
-}
-void display_cell(char cell[8][9])
-{
-    printf("\n");
-    for (int i = 0; i < 8; i++) {
-        printf("\t\t%s\n", cell[i]);
-    }
-}
-/*void display_cell(char cell[8][9])
-{
-    printf("\n");
-    for (int i = 0; i < 8; i++) {
-        printf("\t\t%s\n", cell[i]);
-    }
-}*/
 
 void move(char turn[], int n, char* cell)
 {
--- src/libchess/move.h
+++ src/libchess/move.h
@@ -7,5 +7,3 @@ void swap(char* a, char* b);
 int lit_to_int(char lit);
 void parse(char str[], int n, cord* start, cord* end);
 void move(char turn[], int n, char* cell);
-void start_cell(char cell[][9]);
-void display_cell(char cell[8][9]);
